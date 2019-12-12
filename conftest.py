fb94c72c77a0c262f44cd39e78592b66c8e047d2
tradeurcasl <58733051+tradeurcasl@users.noreply.github.com>
2019-12-12 18:51:44 +0300

    conftest with default en from task 3_6

---
+++ conftest.py
@@ -0,0 +1,19 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default=en,
+                     help="Choose browser language abbrevation")
+    
+@pytest.fixture(scope="function")
+def browser(request):
+    user_language = request.config.getoption("language")
+    browser = None
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
