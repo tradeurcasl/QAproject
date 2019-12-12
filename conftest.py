8828f2013bb315d838ccbfe8f7eca7235d4b3010
tradeurcasl <58733051+tradeurcasl@users.noreply.github.com>
2019-12-12 19:10:27 +0300

    add kavichki to delete mistake

--- conftest.py
+++ conftest.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default=en,
+    parser.addoption('--language', action='store', default="en"",
                      help="Choose browser language abbrevation")
     
 @pytest.fixture(scope="function")
