7bcbca10123ebd1261b6690d590783a76fef3aec
tradeurcasl <58733051+tradeurcasl@users.noreply.github.com>
2019-12-13 14:44:50 +0300

    Update test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -10,4 +10,4 @@ def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
-    assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
+    
