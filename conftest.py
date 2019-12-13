15067e776026172fbf0a1b2982b89dec7038d460
tradeurcasl <58733051+tradeurcasl@users.noreply.github.com>
2019-12-13 12:24:29 +0300

    delete extra opening webbrawser

--- conftest.py
+++ conftest.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default="en"",
+    parser.addoption('--language', action='store', default='en',
                      help="Choose browser language abbrevation")
     
 @pytest.fixture(scope="function")
@@ -13,7 +13,6 @@ def browser(request):
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
     browser = webdriver.Chrome(options=options)
-    browser = webdriver.Chrome()
     yield browser
     print("\nquit browser..")
     browser.quit()
