fa428956456823f2733f023e37a7ea68c6c0ec27
tradeurcasl <58733051+tradeurcasl@users.noreply.github.com>
2019-12-13 13:22:43 +0300

    add func to catch NoSuchElementFound)' git push origin master break '

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,21 @@
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+
 class BasePage():
     def __init__(self, browser, url):
         self.browser = browser
         self.url = url
     def open(self):
         self.browser.get(self.url)
+
+    def __init__(self, browser, url, timeout=10):
+        self.browser = browser
+        self.url = url
+        self.browser.implicitly_wait(timeout)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
+        except (NoSuchElementException):
+            return False
+        return True
