76b46b41d2fa5d463c39ee3ee0221f65216f693b
tradeurcasl <58733051+tradeurcasl@users.noreply.github.com>
2019-12-13 11:50:33 +0300

    delete extra webdriver.Chrome()

--- conftest.py
+++ conftest.py
@@ -13,7 +13,6 @@ def browser(request):
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
     browser = webdriver.Chrome(options=options)
-    browser = webdriver.Chrome()
     yield browser
     print("\nquit browser..")
     browser.quit()
