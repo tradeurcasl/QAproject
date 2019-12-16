e408d0b63678d693773cd701792a35bb007ca31e
tradeurcasl <58733051+tradeurcasl@users.noreply.github.com>
2019-12-16 15:24:22 +0300

    add log and reg selectors

---
+++ pages/locators.py
@@ -0,0 +1,18 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators():
+    LOGIN_LINK = [By.CSS_SELECTOR, '#login_link']
+
+class LoginPageLocators():
+    LOGIN_LINK = [By.CSS_SELECTOR, '#login_link']
+    EMAIL_LOG = [By.CSS_SELECTOR, '#id_login-username']
+    PASSWORD_LOG = [By.CSS_SELECTOR, '#id_login-password']
+    BUTTON_LOG = [By.CSS_SELECTOR, 'button[name="login_submit"]']
+    EMAIL_REG = [By.CSS_SELECTOR,'#id_registration-email']
+    PASS_LOG1 = [By.CSS_SELECTOR, '#id_registration-password1']
+    PASS_LOG2 = [By.CSS_SELECTOR, '#id_registration-password2']
+    BUTTON_REG = [By.CSS_SELECTOR, 'button[name="registration_submit"']
+    LOG_IN = [By.CSS_SELECTOR, '#login_form']
+    REG_IN = [By.CSS_SELECTOR, '#register_form']
+
