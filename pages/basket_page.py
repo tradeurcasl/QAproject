9b8f58e592f46d54ddc164b556072564102627ca
tradeurcasl <58733051+tradeurcasl@users.noreply.github.com>
2019-12-23 17:22:38 +0300

    add links between main page and basket

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -7,3 +7,7 @@ class BasketPage(BasePage):
     def see_product_in_basket_opened_from_product_page(self):
         time.sleep(3)
         assert self.is_element_present(*BasePageLocators.CART_EMPTY), 'Cart is not empty'
+
+    def see_product_in_basket_opened_from_main_page(self):
+        time.sleep(3)
+        assert self.is_element_present(*BasePageLocators.CART_EMPTY), 'Cart is not empty'
