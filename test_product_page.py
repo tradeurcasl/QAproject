d28a26d58ccedf3a0c3e3b4a690a37a5adfc9cb4
tradeurcasl <58733051+tradeurcasl@users.noreply.github.com>
2019-12-23 12:21:56 +0300

    add parametrise for promo-action with different url

--- test_product_page.py
+++ test_product_page.py
@@ -1,13 +1,16 @@
 from .pages.product_page import ProductPage
 import pytest
 from .pages.product_page import ProductPage
+import time
+list_of_pages = a = [x for x in range(10)]
 
-
-def test_guest_can_add_product_to_cart(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('pages', list_of_pages)
+def test_guest_can_add_product_to_cart(browser, pages):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{}".format(pages)
     page = ProductPage(browser, link)
     page.open()
     page.press_button_add_to_basket()
     page.solve_quiz_and_get_code()
+    time.sleep(2)
     page.should_be_message_basket_total()
     page.should_be_message_about_adding()
