b74b2758fa041a6a5c52b6b3b3135c3be8c5ddeb
tradeurcasl <58733051+tradeurcasl@users.noreply.github.com>
2019-12-24 14:51:20 +0300

    fixed bugged test

--- test_product_page.py
+++ test_product_page.py
@@ -45,13 +45,12 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     time.sleep(1)
     page.message_disappeared_after_adding_product_to_basket()
 
-@pytest.mark.need_review('pages', list_of_pages)
-def test_guest_can_add_product_to_basket(browser, pages):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{}".format(pages)
+@pytest.mark.need_review
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)
     page.open()
     page.press_button_add_to_basket()
-    page.solve_quiz_and_get_code()
     time.sleep(4)
     page.should_be_message_basket_total()
     page.should_be_message_about_adding()
